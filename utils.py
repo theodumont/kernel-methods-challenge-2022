32a36adb9600f2e7d934841bd6b8e7ad02745d0b
Théo Dumont <theo.dumont@mines-paristech.fr>
2022-03-25 20:04:24 +0100

    Put data reading in utils

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+def get_data(data_path="./data"):
+    Xtr = np.array(pd.read_csv(os.path.join(data_path, 'Xtr.csv'), header=None, sep=',', usecols=range(3072)))
+    Xte = np.array(pd.read_csv(os.path.join(data_path, 'Xte.csv'), header=None, sep=',', usecols=range(3072)))
+    Ytr = np.array(pd.read_csv(os.path.join(data_path, 'Ytr.csv'), header=None, sep=','))[1:,1].astype(np.int32)
+    return Xtr, Xte, Ytr
+
 
 def array_to_tensor(array):
     """Transform the 5000 x 3072 dataset into 5000 x 32 x 32 x 3."""
@@ -25,3 +34,4 @@ def show_images(tensor, Ytr=None, nb_img=6):
         if Ytr is not None:
             plt.title(f"Image of class {Ytr[i]}")
     plt.show()
+
