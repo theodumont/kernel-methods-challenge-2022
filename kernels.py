f177c79fed8206bc78b10387d4d01a11be466e8b
Théo Dumont <theo.dumont@mines-paristech.fr>
2022-04-07 16:32:32 +0200

    Clean models and kernels and remove old notebook

--- kernels.py
+++ kernels.py
@@ -1,20 +1,20 @@
+"""Kernels implemented:
+- Gaussian kernel
+- RBF kernel
+- TStudent kernel
+- linear kernel
+- polynomial kernel
+- Chi2 kernel
+- wavelet kernel
+- log kernel
+- GHI kernel
+"""
 import numpy as np
 import torch
-from scipy.spatial import distance_matrix
 
-
-class GaussianKernel_old:
-    """Gaussian kernel.
-
-        K(x, y) = exp(-||x-y||^2/(2*sigma^2))
-    """
-    def __init__(self, sigma=1.):
-        self.sigma = sigma
-    def kernel(self,X,Y):
-        return np.exp(-distance_matrix(X,Y)/(2*self.sigma**2))
 
 class GaussianKernel:
-    """Gaussian kernel using torch for faster computation.
+    """Gaussian kernel.
 
         K(x, y) = exp(-||x-y||/(2*sigma^2))
     """
@@ -26,7 +26,7 @@ class GaussianKernel:
         return np.array(torch.exp(-torch.cdist(X_,Y_)/(2*self.sigma**2)))
 
 class RBFKernel:
-    """RBF kernel using torch for faster computation.
+    """RBF kernel.
 
         K(x, y) = exp(-g||x-y||^2)
     """
@@ -38,7 +38,7 @@ class RBFKernel:
         return np.array(torch.exp(-torch.cdist(X_,Y_)**2/(2*self.sigma**2)))
 
 class TStudentKernel:
-    """T-Student kernel "Alternative Kernels for Image Recognition".
+    """T-Student kernel.
 
         K(x, y) = 1 / (1 + ||x - y||^p)
     """
@@ -48,7 +48,6 @@ class TStudentKernel:
         X_ = torch.tensor(X)
         Y_ = torch.tensor(Y)
         return np.array(1 / (1 + torch.cdist(X_,Y_)**self.p))
-
 
 class LinearKernel:
     """Linear kernel.
@@ -72,43 +71,46 @@ class PolynomialKernel:
 
 class Chi2Kernel:
     """
-    Chi^2 kernel,
-        K(x, y) = exp( -gamma * SUM_i (x_i - y_i)^2 / (x_i + y_i) )
-    as defined in:
-    "Local features and kernels for classification
-     of texture and object categories: A comprehensive study"
-    Zhang, J. and Marszalek, M. and Lazebnik, S. and Schmid, C.
-    International Journal of Computer Vision 2007
+    Chi^2 kernel.
+
+        K(x, y) = exp( -gamma * sum_i (x_i - y_i)^2 / (x_i + y_i) )
+
+    "Local features and kernels for classification of texture and object categories: A
+    comprehensive study", Zhang et al, 2006
     http://eprints.pascal-network.org/archive/00002309/01/Zhang06-IJCV.pdf
     """
 
     def __init__(self, gamma=1.):
         self._gamma = gamma
-
     def kernel(self, X, Y):
-
         if np.any(X < 0) or np.any(Y < 0):
             print('Chi^2 kernel requires data to be strictly positive!')
-
         kernel = np.zeros((X.shape[0], Y.shape[0]))
-
         for d in range(X.shape[1]):
-            column_1 = X[:, d].reshape(-1, 1)
-            column_2 = Y[:, d].reshape(-1, 1)
-            kernel += (column_1 - column_2.T)**2 / (column_1 + column_2.T)
-
+            kernel += (X[:, d].reshape(-1, 1) - Y[:, d].reshape(-1, 1).T)**2 / (X[:, d].reshape(-1, 1) + Y[:, d].reshape(-1, 1).T)
         return np.exp(-self._gamma * kernel)
 
 class WaveletKernel:
-    """http://see.xidian.edu.cn/faculty/zhangli/publications/WSVM.pdf"""
+    """
+    Wavelet kernel.
+
+        K(x, y) = prod_i h( (x_i - y_i) / a)
+        with h(t) = cos(1.75t) exp(-t^2/2)
+
+    "Wavelet support vector machine", Zhang et al, 2004
+    http://see.xidian.edu.cn/faculty/zhangli/publications/WSVM.pdf
+    """
     def __init__(self, a=1):
         self.a = a
         self.h = lambda t: np.cos(1.75*t) * np.exp(-t**2/2)
     def kernel(self,X,Y):
-        return np.prod(self.h((X-Y)/self.a))
+        K = np.ones((X.shape[0], Y.shape[0]))
+        for d in range(X.shape[1]):
+            K *= self.h( (X[:, d].reshape(-1, 1) - Y[:, d].reshape(-1, 1).T) / self.a )
+        return K
 
 class LogKernel:
-    """Log kernel using torch for faster computation.
+    """Log kernel.
 
         K(x, y) = - log(1+||x-y||^d)
     """
@@ -124,6 +126,7 @@ class GHIKernel:
 
         K(x, y) = sum_i min(|x_i|^beta,|y_i|^beta|)
 
+    "Generalized histogram intersection kernel for image recognition", Boughorbel et al, 2005
     http://perso.lcpc.fr/tarel.jean-philippe/publis/jpt-icip05.pdf
     """
     def __init__(self, beta=1.):
--- models.py
+++ models.py
@@ -1,7 +1,18 @@
+"""Models implemented:
+- One-vs-rest KRR (Kernel Ridge Regression)
+- One-vs-rest KSVC (Kernel Support Vector Classifier)
+- Kernel PCA
+"""
 import numpy as np
 from tqdm import tqdm
+from scipy.linalg import eigh
+from tqdm import tqdm
+from scipy import optimize
+
+# KRR ====================================================================================
 
 class KRR:
+    """Kernel Ridge Regression."""
 
     def __init__(self, kernel, class_id, lambd, verbose):
         self.kernel = kernel
@@ -13,16 +24,16 @@ class KRR:
     def fit(self, y, K):
         if self.verbose: print("Fitting...")
         n = len(y)
-        # self.X = X
         self.y = (y == self.class_id)
-        self.alpha = np.linalg.inv(K + self.lambd * n * np.eye(n)) @ self.y
+        self.alpha = np.linalg.solve(K + self.lambd * n * np.eye(n), self.y)
 
     def predict(self, K):
         if self.verbose: print("Predicting...")
         pred = K @ self.alpha
         return pred
 
-class MultiKRR:
+class OneVsRestKRR:
+    """One-vs-rest KRR."""
 
     def __init__(self, kernel, lambd=1e-3, verbose=False):
         self.kernel = kernel
@@ -41,3 +52,101 @@ class MultiKRR:
         pred = np.array([krr.predict(K) for krr in self.KRR_list]).T
         pred = np.argmax(pred, axis=1)
         return pred
+
+
+# KSVC ===================================================================================
+
+class KSVC:
+    """Kernel Support Vector Classifier."""
+
+    def __init__(self, kernel, C, class_id, epsilon, verbose):
+        self.kernel = kernel
+        self.C = C
+        self.alpha = None
+        self.support = None
+        self.epsilon = epsilon
+        self.norm_f = None
+        self.verbose = verbose
+        self.class_id = class_id
+
+    def fit(self, X, y, K):
+        if self.verbose: print("Fitting...")
+        N = len(y)
+        self.X = X
+        self.y = (y == self.class_id)
+
+        # Lagrange dual problem
+        def loss(alpha):
+            return 1/2 * alpha.T @ np.diag(y) @ K @ np.diag(y) @ alpha - alpha.sum()
+        # Partial derivate of Ld on alpha
+        def grad_loss(alpha):
+            return np.diag(y) @ K @ np.diag(y) @ alpha - 1
+
+        fun_eq = lambda alpha: - alpha.T @ y
+        jac_eq = lambda alpha: - y
+        fun_ineq = lambda alpha: self.C - alpha
+        jac_ineq = lambda alpha: - np.eye(N)
+        fun_ineq_pos = lambda alpha: alpha
+        jac_ineq_pos = lambda alpha: np.eye(N)
+
+        constraints = ({'type': 'eq',  'fun': fun_eq, 'jac': jac_eq},
+                       {'type': 'ineq', 'fun': fun_ineq , 'jac': jac_ineq},
+                       {'type': 'ineq', 'fun': fun_ineq_pos, 'jac': jac_ineq_pos})
+
+        optRes = optimize.minimize(fun=lambda alpha: loss(alpha),
+                                   x0=np.ones(N),
+                                   method='SLSQP',
+                                   jac=lambda alpha: grad_loss(alpha),
+                                   constraints=constraints)
+        self.alpha = optRes.x
+
+        self.supportIndices = np.where((self.alpha > self.epsilon) & (self.alpha < self.C*np.ones(N) - self.epsilon))
+        self.support = X[self.supportIndices]
+        self.b = (self.y - self.alpha.T @ np.diag(self.y) @ K)[self.supportIndices].mean()
+        self.norm_f = self.alpha.T @ np.diag(self.y) @ K @ np.diag(self.y) @ self.alpha
+
+    def predict(self, K):
+        if self.verbose: print("Predicting...")
+        d = self.alpha.T @ np.diag(self.y) @ K
+        return d + self.b
+
+class OneVsRestKSVC:
+    """One-vs-rest KSVC."""
+
+    def __init__(self, kernel, C, epsilon=1e-3, verbose=False):
+        self.C = C
+        self.kernel = kernel
+        self.epsilon = epsilon
+        self.verbose = verbose
+        self.KSVC_list = [KSVC(self.kernel, self.C, self.epsilon, class_id, self.verbose) for class_id in range(10)]
+
+    def fit(self, X, y):
+        self.X = X
+        K = self.kernel(X,X)
+        for ksvc in tqdm(self.KSVC_list, desc="Fitting"):
+            ksvc.fit(X, y, K)
+
+    def predict(self, x):
+        K = self.kernel(self.X, x)
+        pred = np.array([ksvc.predict(K) for ksvc in self.KSVC_list]).T
+        pred = np.argmax(pred, axis=1)
+        return pred
+
+
+# KPCA ===================================================================================
+
+def kernel_pca(K, n_components):
+    """Following slide 219/1013 of the course."""
+    # 1. center Gram matrix
+    N = K.shape[0]
+    one_n = np.ones((N,N)) / N
+    K = K - one_n @ K - K @ one_n + one_n @ K @ one_n
+    # 2. compute the first eigenvectors (u_i, \Delta_i)
+    Delta, u = eigh(K)  # ascending order
+    Delta, u = Delta[::-1], u[:,::-1]  # descending order
+    Delta, u = Delta[:n_components], u[:,:n_components]  # first eigs
+    # 3. normalize the eigenvectors \alpha_i = u_i / \sqrt{\Delta_i}
+    alpha = u / Delta
+    # 4. return projections K @ alpha
+    Xtr_proj = K @ alpha
+    return Xtr_proj
